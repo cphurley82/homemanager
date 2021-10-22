456f4b2da0f05c9b0739cf22db52bdf9772b681b
Chris Hurley <cphurley82@gmail.com>
2021-10-22 16:53:43 -0700

    Intial documentation. Fixes #1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # homemanager
+
 Makes managing technology at home easier.
+
+Currently all this does is ping a list of hosts and shuts them down if they
+respond.
+
+## Setup
+
+homemanager requires Python 3.6 or above and [some packages](requirements.txt)
+which can be installed with pip like so:
+
+```bash
+pip install --requirement requirements.txt
+```
+
+homemanger is configured by creating a file called `config.json` in the working
+direcoty. It looks like this:
+
+```json
+{
+    "host_names": [
+        // Domnain names or IP addresses of the hosts being managed.
+        "computer1.local",
+        "192.168.42.42"
+    ]
+}
+```
+
+homemanger requires a password to be stored in the system keyring like so:
+
+```bash
+python -m keyring set system homemanager
+```
+
+Run homemanger like so:
+
+```bash
+python3 homemanger.py
+```
--- homemanager.py
+++ homemanager.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""homemanger makes managing home computers easier.
+
+Currently all this does is ping the host and shuts it down if it responds.
+
+Example:
+    $ python3 homemanager.py
+"""
 import paramiko
 import ping3
 import keyring
@@ -5,16 +13,20 @@ import commentjson
 
 
 def get_host_names():
+    """Returns a list of host names read from configuration file."""
     with open('config.json') as f:
         data = commentjson.load(f)
     return data['host_names']
 
 
 def get_password():
-    return keyring.get_password('system', 'homeadmin')
+    """Returns the password from the keyring."""
+    return keyring.get_password('system', 'homemanager')
 
 
 def main():
+    """Main function."""
+    # For each host, ping it and shutdown if it responds.
     for host_name in get_host_names():
         print(f'Pinging {host_name}...', end='')
         response = ping3.ping(dest_addr=host_name)
@@ -22,26 +34,34 @@ def main():
             print(f'no response.')
         else:
             print(f'resonse in {response} secs, shutting it down...')
+
+            # SSH into the host using the password saved in the keyring.
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             client.connect(hostname=host_name,
                            username='chris',
                            password=get_password())
+
+            # Execute the shutdown command.
             stdin, stdout, stderr = client.exec_command(
                 'sudo -S shutdown -h now')
 
+            # Provide the password to sudo.
             stdin.write(get_password() + "\n")
             stdin.flush()
 
+            # Print the output from the shutdown command.
+            # Sometimes the connection gets killed before anything comes out
+            # so it prints nothing but still shuts down.
             print(f'STDOUT: {stdout.read().decode("utf8")}')
-            # print(f'STDERR: {stderr.read().decode("utf8")}')
 
-            # Because they are file objects, they need to be closed
+            # Close the SSH connection.
+            client.close()
+
+            # Close file objects.
             stdin.close()
             stdout.close()
             stderr.close()
-
-            client.close()
 
 
 if __name__ == '__main__':
