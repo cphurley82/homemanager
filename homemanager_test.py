97cc7634cc317e35ae9170e8b577c20ea9bcfbfc
cphurley82 <cphurley82@gmail.com>
2021-10-22 23:24:37 -0700

    Made some working unit tests.

--- homemanager_test.py
+++ homemanager_test.py
@@ -1,5 +1,25 @@
 import homemanager
+import io
+import keyring
 
 
-def test_doesnt_break():
-    homemanager.main()
+def test_get_host_names():
+    assert homemanager.get_host_names_from_file(file=io.StringIO('''
+        {
+        "host_names": [
+            // Domain names or IP addresses of the hosts being managed.
+            "computer1.local",
+            "192.168.42.42"
+        ]
+        }
+        ''')) == ['computer1.local', '192.168.42.42']
+
+
+def test_get_password():
+    keyring.set_password('system', 'homemanager', 'password')
+    assert homemanager.get_password() == 'password'
+
+
+def test_ping_host():
+    assert homemanager.ping_host('localhost') == True
+    assert homemanager.ping_host('not.a.real.host') == False
